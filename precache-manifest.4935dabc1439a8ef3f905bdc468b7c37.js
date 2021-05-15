858718c15bbb33fd2abf91cd028d5dbaeda3d2b8
root <root@NFrag-Home-Desktop.localdomain>
2021-05-15 15:55:59 -0700

    Updates

---
+++ precache-manifest.4935dabc1439a8ef3f905bdc468b7c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f43e8c62c179f4a416586d6d581a11e",
+    "url": "/React-Hacker_News/index.html"
+  },
+  {
+    "revision": "bc022e89d78e428b0bdf",
+    "url": "/React-Hacker_News/static/css/main.9c59207b.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/React-Hacker_News/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Hacker_News/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc022e89d78e428b0bdf",
+    "url": "/React-Hacker_News/static/js/main.7cc060b3.chunk.js"
+  },
+  {
+    "revision": "dc5066df035b2b3de59e",
+    "url": "/React-Hacker_News/static/js/runtime-main.abef0f4c.js"
+  }
+]);
