3f0f7879498e11d7918999f0860cad817fa2ce2d
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-05-15 12:17:59 -0700

    Update context.js
    - Created state and dispatch from useReducer
    - Setup initial state with loading property
    - Created fetchStories function to run on initial render via useEffect

--- src/context.js
+++ src/context.js
@@ -1,26 +1,32 @@
-import React, { useContext, useEffect, useReducer } from 'react'
+import React, { useContext, useEffect, useReducer } from 'react';
 
-import {
-  SET_LOADING,
-  SET_STORIES,
-  REMOVE_STORY,
-  HANDLE_PAGE,
-  HANDLE_SEARCH,
-} from './actions'
-import reducer from './reducer'
+import { SET_LOADING, SET_STORIES, REMOVE_STORY, HANDLE_PAGE, HANDLE_SEARCH } from './actions';
+import reducer from './reducer';
 
-const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?';
 
-const initialState = {}
+const initialState = {
+	isLoading: true
+};
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	const fetchStories = async (url) => {
+		dispatch({ type: SET_LOADING });
+	};
+
+	useEffect(() => {
+		fetchStories();
+	}, []);
+
+	return <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>;
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+	return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
