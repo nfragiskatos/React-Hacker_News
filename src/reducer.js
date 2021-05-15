0a2f3b5eb0da08f81dc75149323d51189892b91b
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-05-15 12:26:08 -0700

    Initial Loading implementation
    App.js
    - Added base UI components
    
    Stories.js
    - Retrieved isLoading from context state and conditionally rendered loading ui.
    
    reducer.js
    - Added initial type check for loading action, as well as case for non matching type.

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,14 @@
-import {
-  SET_LOADING,
-  SET_STORIES,
-  REMOVE_STORY,
-  HANDLE_PAGE,
-  HANDLE_SEARCH,
-} from './actions'
+import { SET_LOADING, SET_STORIES, REMOVE_STORY, HANDLE_PAGE, HANDLE_SEARCH } from './actions';
 
-const reducer = () => {}
-export default reducer
+const reducer = (state, action) => {
+	switch (action.type) {
+		case SET_LOADING:
+			return { ...state, isLoading: true };
+			break;
+
+		default:
+			throw new Error(`no matching ${action.type} action`);
+			break;
+	}
+};
+export default reducer;
